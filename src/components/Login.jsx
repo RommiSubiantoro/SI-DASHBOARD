e537824fe4ea6f2cba5238395eb47b66f8f1a168
RommiSubiantoro <rommisubiantoro@gmail.com>
2025-08-21 15:22:59 +0800

    ini projek magang dashboard

---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import loginImage from "../assets/truck.png"; 
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  return (
+      <div className="container">
+        <div className="auth">
+          <form>
+             <h1 className="login">Login</h1>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={(e) => setFormData({...formData, username: e.target.value})}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={(e) => setFormData({...formData, password: e.target.value})}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        
+        <div className="logo-login">
+          {
+
+          }
+          <img src={loginImage} />
+        </div>
+      </div>
+  );
+};
+
+export default Login;
